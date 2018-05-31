76c87d4e95bc472f67c1356ed4a32e19550b7b8d
Sakshi Gokhru <sakshi@jalantechnologies.com>
2018-05-31 17:49:41 +0530

    ERC20 token integration with JTCRET token

--- contracts/JTCRET.sol
+++ contracts/JTCRET.sol
@@ -1,31 +1,118 @@
 pragma solidity ^0.4.21;
-import './ERC20.sol';
 
-contract JTCRET is ERC20 {
-  address public owner;
-  string public propertyAddress;
-  struct OwnerDetails {
-    string email;
-    string deedHash;
-  }
-  OwnerDetails[] public ownerDetails;
+// ----------------------------------------------------------------------------
+// 'JTCRET' token contract
+//
+// Deployed to : 0x90D8bf4f1ed98F3Ee5699eE3a591721dEE2E4341
+// Symbol      : JTCRET
+// Name        : JTCRET
+//
+// ----------------------------------------------------------------------------
 
-  function JTCRET() public {
-    owner = msg.sender;
-  }
 
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-  function generateJTCRETToken(string _propertyAddress) public returns(bool success) {
-      require(bytes(_propertyAddress).length > 0);
-      return true;
-  }
-  function transferFrom(address _from, address _to, uint _value) public  returns (bool success) {
-      return true;
-  }
-  function approve(address _spender, uint _value) public returns (bool success) {
-      return true;
-  }
+// ----------------------------------------------------------------------------
+// Safe maths
+// ----------------------------------------------------------------------------
+contract SafeMath {
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+
+
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+}
+
+
+// ----------------------------------------------------------------------------
+// Owned contract
+// ----------------------------------------------------------------------------
+contract Owned {
+    address public owner;
+    address public newOwner;
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    function Owned() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
+}
+
+
+// ----------------------------------------------------------------------------
+// ERC20 Token, with the addition of symbol, name and decimals and assisted
+// token transfers
+// ----------------------------------------------------------------------------
+contract JTCRET is ERC20Interface, Owned, SafeMath {
+    string public propertyAddress;
+    struct OwnerDetails {
+      string email;
+      string deedHash;
+    }
+    OwnerDetails[] public ownerDetails;
+
+
+    // ------------------------------------------------------------------------
+    // Constructor
+    // ------------------------------------------------------------------------
+    function JTCRET() public {
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Transfer tokens from the from account to the to account
+    //
+    // The calling account must already have sufficient tokens approve(...)-d
+    // for spending from the from account and
+    // - From account must have sufficient balance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        Transfer(from, to, tokens);
+        return true;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Don't accept ETH
+    // ------------------------------------------------------------------------
+    function () public payable {
+        revert();
+    }
+
 }
