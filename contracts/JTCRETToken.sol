d70c1716417bd0bedeb4a36d4db37072ad1a2cc9
Sakshi Gokhru <sakshi@jalantechnologies.com>
2018-06-13 19:19:07 +0530

    Improve create property method changes, save owner details as transaction metadata

--- contracts/JTCRETToken.sol
+++ contracts/JTCRETToken.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.4.4;
+pragma experimental ABIEncoderV2;
 
 import "./StandardToken.sol";
 
@@ -12,6 +13,22 @@ contract JTCRETToken is StandardToken {
     string public name;
     uint8 public decimals;
     string public symbol;
+
+    struct Property {
+        uint id;
+        string propertyAddress;
+        address[] ownerDetails;
+    }
+
+    struct OwnerDetails {
+        string ownerName;
+        string ownerEmail;
+        string deedURL;
+        address propertyAddress;
+    }
+    mapping (uint => Property) public property;
+    mapping (address => OwnerDetails) public ownerDetails;
+    uint totalProperties;
 
     function JTCRETToken(address _owner, string _tokenName, string _tokenSymbol) public {
         balances[_owner] = 100000000000000000000000000;
@@ -33,16 +50,52 @@ contract JTCRETToken is StandardToken {
         return true;
     }
 
-    function createProperty(address _to, uint256 _value, string _propertyAddress) public returns (bool success) {
+    function createProperty(address _to, uint256 _value, string _propertyAddress, string _ownerName, string _ownerEmail, string _deedURL) public returns (bool success) {
         //Default assumes totalSupply can't be over max (2^256 - 1).
         //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
         //Replace the if with this one instead.
         if (balances[msg.sender] >= _value && _value > 0) {
             balances[msg.sender] -= _value;
             balances[_to] += _value;
-            propertyAddress[_to] = _propertyAddress;
+            totalProperties++;
+            property[totalProperties].id = totalProperties;
+            property[totalProperties].propertyAddress = _propertyAddress;
+            ownerDetails[_to].ownerName = _ownerName;
+            ownerDetails[_to].ownerEmail = _ownerEmail;
+            ownerDetails[_to].deedURL = _deedURL;
+            property[totalProperties].ownerDetails.push(_to);
             emit Transfer(msg.sender, _to, _value);
             return true;
         } else { return false; }
+    }
+
+    function getPropertyIds() public constant returns (uint[] _propertyId) {
+        // we check whether there is at least one property
+        require(totalProperties > 0);
+        uint[] memory allPropertyIds = new uint[](totalProperties);
+        uint numberOfProperties = 0;
+        for (uint i = 1; i <= totalProperties; i++) {
+            allPropertyIds[numberOfProperties] = i;
+            numberOfProperties ++;
+        }
+        return allPropertyIds;
+    }
+
+    function getPropertyOwnerDetails(uint _propertyId) public constant returns (
+        string _propertyAddress,
+        string[] _ownerName,
+        string[] _ownerEmail,
+        string[] _deedURL) {
+        Property memory _property = property[_propertyId];
+        string[] memory ownersNames = new string[](_property.ownerDetails.length);
+        string[] memory ownersEmails = new string[](_property.ownerDetails.length);
+        string[] memory deedURLS = new string[](_property.ownerDetails.length);
+        for (uint i = 0; i < _property.ownerDetails.length; i++) {
+            OwnerDetails memory ownerDetail = ownerDetails[_property.ownerDetails[i]];
+            ownersNames[i] = ownerDetail.ownerName;
+            ownersEmails[i] = ownerDetail.ownerEmail;
+            deedURLS[i] = ownerDetail.deedURL;
+        }
+        return (_property.propertyAddress, ownersNames, ownersEmails, deedURLS);
     }
 }
