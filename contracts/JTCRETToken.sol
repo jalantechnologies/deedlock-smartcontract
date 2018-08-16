748943997d5c16d56aa0b139336d6d77a6f544ac
Sakshi Gokhru <sakshi@jalantechnologies.com>
2018-08-16 10:44:07 +0530

    Added initial ERC-721 token interface files

--- contracts/JTCRETToken.sol
+++ contracts/JTCRETToken.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.4;
 
-import "./StandardToken.sol";
+import "./ERC721Token.sol";
 
-contract JTCRETToken is StandardToken {
+contract JTCRETToken is ERC721Token {
 
     function () public {
         //if ether is sent to this address, send it back.
@@ -29,7 +29,7 @@ contract JTCRETToken is StandardToken {
     event PropertyTokenTransferred(address indexed _to, string propertyAddress);
 
     function JTCRETToken(address _owner, string _tokenName, string _tokenSymbol) public {
-        balances[_owner] = 100000000000000000000000000;
+        balanceOf[_owner] = 100000000000000000000000000;
         totalSupply = 100000000000000000000000000;
         name = _tokenName;
         symbol = _tokenSymbol;
@@ -52,9 +52,9 @@ contract JTCRETToken is StandardToken {
         //Default assumes totalSupply can't be over max (2^256 - 1).
         //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
         //Replace the if with this one instead.
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
+        if (balanceOf[msg.sender] >= _value && _value > 0) {
+            balanceOf[msg.sender] -= _value;
+            balanceOf[_to] += _value;
             propertyAddress[_to] = _propertyAddress;
             propertyTransferDetails[_propertyAddress].push(PropertyTransferInfo(Owner(_ownerName, _ownerEmailAddress, _to), ""));
             emit PropertyTokenCreated(_to, _propertyAddress);
@@ -80,13 +80,13 @@ contract JTCRETToken is StandardToken {
     }
 
     function transferProperty(address _to, uint256 _value, string _propertyAddress, string _ownerName, string _ownerEmailAddress, string _deedURL) public returns (bool success) {
-        if (balances[msg.sender] >= _value && _value > 0) {
+        if (balanceOf[msg.sender] >= _value && _value > 0) {
             require(propertyTransferDetails[_propertyAddress].length > 0);
             uint currentOwnerIndex = propertyTransferDetails[_propertyAddress].length - 1;
             require(propertyTransferDetails[_propertyAddress][currentOwnerIndex].owner.walletAddress == msg.sender);
             require(bytes(_deedURL).length > 0);
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
+            balanceOf[msg.sender] -= _value;
+            balanceOf[_to] += _value;
             propertyAddress[_to] = _propertyAddress;
             propertyTransferDetails[_propertyAddress].push(PropertyTransferInfo(Owner(_ownerName, _ownerEmailAddress, _to), _deedURL));
             emit PropertyTokenTransferred(_to, _propertyAddress);
